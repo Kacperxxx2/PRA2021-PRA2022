16b152b932b2a8069fc266969c1c174d5c22a5e1
Kacper Pękalski <kac.pekalski1@gmail.com>
2021-10-05 10:39:54 +0200

    IntroductionClassesWithLogger

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,15 @@
 package introduction;
 
+import java.util.logging.Logger;
+
 public class HelloWorld {
+
+    public static Logger log = Logger.getLogger("name");
 
     public static void main(String [ ] args) {
         System.out.println("Hello! Witaj!");
+
+        log.info("message");
+
     }
 }
